0279ad0e5882653f986cd34a2683ad587aee087c
Aaron <kammingwai@Aarons-MacBook-Air.local>
2026-04-13 18:29:18 +0800

    Remove custom PWA icon metadata

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'marriott-v2026.04.13.11';
+const CACHE = 'marriott-v2026.04.13.12';
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json', './icon-192.png']))
+    caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json']))
   );
   self.skipWaiting();
 });
